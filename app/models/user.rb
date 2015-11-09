91f14138509e260803b39ece4a50810dccb6e01d
holyshatots <themcaffee@gmail.com>
2015-11-08 20:56:08 -0700

    Enabling confirmation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
