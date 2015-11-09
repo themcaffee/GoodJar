9aaab5ead0eead0700a12035a2e88b9679c9287c
holyshatots <themcaffee@gmail.com>
2015-11-08 20:48:42 -0700

    Date helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+
+  require 'date'
+
+  def date_of_next(day)
+    date  = Date.parse(day)
+    delta = date > Date.today ? 0 : 7
+    date + delta
+  end
+
   def full_image_path(img_path)
     request.protocol + request.host_with_port + image_path(img_path)
   end
