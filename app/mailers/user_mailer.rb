a12b5124b9db341900453663a0b2580716b64d24
holyshatots <themcaffee@gmail.com>
2015-11-08 21:28:51 -0700

    Unadded files

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+
+  def weekly_email(user)
+
+  end
+
+  def monthly_email(user)
+
+  end
+
+  def yearly_email(user)
+
+  end
+end
---
+++ test/mailers/previews/usre_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UsreMailerPreview < ActionMailer::Preview
+
+end
